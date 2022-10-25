6a4d0a477db4b4fc09a76195a0e6067b4c0976e0
jswob <jacswob@gmail.com>
2022-10-25 17:41:39 +0200

    chore: update accounts functions for new flow

--- lib/szybki_kalendarz_api/accounts.ex
+++ lib/szybki_kalendarz_api/accounts.ex
@@ -8,54 +8,70 @@ defmodule SzybkiKalendarzApi.Accounts do
 
   alias SzybkiKalendarzApi.Accounts.Manager
   alias SzybkiKalendarzApi.Accounts.Congregation
+  alias SzybkiKalendarzApi.Accounts.GoogleUser
 	alias Ueberauth.Auth
 	alias Ueberauth.Auth.Info
 
-	def get_manager_by_email(email) do
-    case Repo.get_by(Manager, email: email) do
-			%Manager{} = manager -> {:ok, manager}
-
-			nil -> {:error, :not_found}
+	def get_google_user_by_email(email) do
+		case Repo.all(
+			from gu in GoogleUser,
+				where: gu.email == ^email
+		) do
+			[user | _] -> {:ok, user}
+			[] -> {:error, :not_found}
 		end
   end
 
-	def create_manager(attrs \\ %{}) do
+	def create_account_from_google_user({:ok, %GoogleUser{type: type} = owner_data}) do
+		{:ok, %{owner: google_user}} =
+			case type do
+				:manager -> create_manager_from_google_user(owner_data)
+
+				:congregation -> create_congregation_from_google_user(owner_data)
+			end
+
+		{:ok, google_user}
+	end
+
+	def create_manager_from_google_user(%GoogleUser{} = owner) do
     %Manager{}
-    |> Manager.changeset(attrs)
+    |> Manager.changeset(%{owner: owner})
     |> Repo.insert()
   end
 
-	def get_congregation_by_email(email) do
-    case Repo.get_by(Congregation, email: email) do
-			%Congregation{} = congregation -> {:ok, congregation}
-
-			nil -> {:error, :not_found}
-		end
-  end
-
-	def create_congregation(attrs \\ %{}) do
+	def create_congregation_from_google_user(%GoogleUser{} = owner) do
     %Congregation{}
-    |> Congregation.changeset(attrs)
+    |> Congregation.changeset(%{owner: owner})
     |> Repo.insert()
   end
 
-	def find_or_create_account_from_auth("manager", %Auth{info: %Info{email: email, image: avatar_url}}) do
-		case get_manager_by_email(email) do
-			{:ok, user} -> {:ok, user}
+	def create_google_user(attrs \\ %{}) do
+		%GoogleUser{}
+		|> GoogleUser.changeset(attrs)
+		|> Repo.insert()
+	end
+
+	def find_or_create_account_from_auth(type, %Auth{info: %Info{email: email, image: avatar_url}}) do
+		case get_google_user_by_email(email) do
+			{:ok, user} -> check_account_type(user, type)
 
 			{:error, :not_found} ->
-				%{email: email, avatar_url: avatar_url}
-				|> create_manager()
+				Repo.transaction(fn ->
+					%{email: email, avatar_url: avatar_url, type: type}
+					|> create_google_user()
+					|> create_account_from_google_user()
+				end)
 		end
 	end
 
-	def find_or_create_account_from_auth("congregation", %Auth{info: %Info{email: email}}) do
-		case get_congregation_by_email(email) do
-			{:ok, congregation} -> {:ok, congregation}
+	defp check_account_type(%GoogleUser{type: account_type} = account, type) do
 
-			{:error, :not_found} ->
-				%{email: email, name: "Nowy Zbór: #{email}"}
-				|> create_congregation()
+		dbg(account_type)
+		cond do
+			String.to_atom(type) == account_type ->
+				{:ok, account}
+
+			true -> {:error, :wrong_account_type}
 		end
 	end
 end
